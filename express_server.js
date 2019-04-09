d3dbe8ee49797e2af74dbeda5fcf9f05be5d5fd6
procadiana <diana.claudia.ilinca@gmail.com>
2019-04-09 22:41:22 +0000

    Added input url and function generating random number

--- express_server.js
+++ express_server.js
@@ -1,13 +1,34 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+
+function generateRandomString() {
+  let randomURL = Math.random().toString(36).replace('0.', '').slice(0.5);
+}
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
